01333b4a10fb3943d7124f7ecb4947a46b77891f
Israel David <israeldiv100@gmail.com>
2024-08-30 23:28:02 -0300

    focus
    make the camera focus only  in the required color

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ while True:
     #give the frame high saturation
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, yellow_lower, yellow_upper)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area > 300:
+            cv2.drawContours(frame, c, -1, (0, 255, 0), 2)
+   
     cv2.imshow('frame', frame)
-    # the mask is to detect the color specified in range
-    cv2.imshow('mask', mask)
     if cv2.waitKey(10)  == ord('q'):
         break
 
